ec9b700d897dfb44b5e73849b458d3a408220df8
Vinícius Henriques <viniciuspinholi6@gmail.com>
2025-04-04 22:24:17 -0300

    Initial Migration and DbInitializer.cs

--- src/ReStore.API/Data/StoreContext.cs
+++ src/ReStore.API/Data/StoreContext.cs
@@ -6,7 +6,7 @@ namespace ReStore.API.Data;
 
 public class StoreContext(DbContextOptions options) : DbContext(options)
 {
-   public required DbSet<Product> Produtos { get; set; }
+   public required DbSet<Product> Products { get; set; }
 
    
 }
