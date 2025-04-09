6399e9c38e451f00b86ea02d06492d61335728ab
Vinícius Henriques <viniciuspinholi6@gmail.com>
2025-04-09 11:56:28 -0300

    useState hook - Add Products to List

--- src/restore-client/src/App.tsx
+++ src/restore-client/src/App.tsx
@@ -1,10 +1,15 @@
-const products = [
-  {name: 'product1', price:100.00},
-  {name: 'product2', price:200.00}, 
-  {name: 'product3', price:300.00}, 
-]
+import { useState } from "react";
+
 
 function App() {
+const [products, setProducts] = useState([
+  {name: 'product1', price:100.00},
+  {name: 'product2', price:200.00}, 
+]);
+
+const addProducts = () => {
+  setProducts(prevState =>  [...prevState, {name: 'product' + (prevState.length + 1), price: (prevState.length * 100) + 100}])
+}
 
   return (
       <>
@@ -14,6 +19,7 @@ function App() {
             <li key={index}> {item.name} - {item.price}</li>
           ))}
         </ul>
+        <button onClick={addProducts}>Add Product</button>
       </>
   )
 }
