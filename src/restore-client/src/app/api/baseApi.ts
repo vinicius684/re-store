05a80cd173ae0534bf24210a5cf816535533c842
Vinícius Henriques <viniciuspinholi6@gmail.com>
2025-04-28 14:24:32 -0300

    Server error component; Not found error component

--- src/restore-client/src/app/api/baseApi.ts
+++ src/restore-client/src/app/api/baseApi.ts
@@ -1,6 +1,7 @@
 import { BaseQueryApi, FetchArgs, fetchBaseQuery } from "@reduxjs/toolkit/query";
 import { startLoading, stopLoading } from "../layout/uiSlice";
 import { toast } from "react-toastify";
+import { router } from "../routes/Routes";
 
 const customBaseQuery = fetchBaseQuery({
     baseUrl: 'https://localhost:5001/api'
@@ -40,11 +41,11 @@ export const baseQueryWithErrorHandling = async (args: string | FetchArgs, api: 
                 break;
             case 404:
                 if(typeof responseData === 'object' && 'title' in responseData)
-                    toast.error(responseData.title);
+                    router.navigate('/not-found')
                 break;
             case 500:
-                if(typeof responseData === 'object' && 'title' in responseData)
-                    toast.error(responseData.title);
+                if(typeof responseData === 'object')
+                    router.navigate('/server-error', {state: {error: responseData}})
                 break;
             default:
                 break;
--- src/restore-client/src/app/routes/Routes.tsx
+++ src/restore-client/src/app/routes/Routes.tsx
@@ -1,10 +1,12 @@
-import { createBrowserRouter } from "react-router-dom";
+import { createBrowserRouter, Navigate } from "react-router-dom";
 import App from "../layout/App";
 import Catalog from "../../features/catalog/Catalog";
 import HomePage from "../../features/home/HomePage";
 import ProductDetails from "../../features/catalog/ProductDetails";
 import AboutPage from "../../features/about/AboutPage";
 import ContactPage from "../../features/contact/ContactPage";
+import ServerError from "../errors/ServerError";
+import NotFound from "../errors/NotFound";
 
 
 export const router = createBrowserRouter([
@@ -17,6 +19,9 @@ export const router = createBrowserRouter([
             { path: '/catalog/:id', element: <ProductDetails /> },
             { path: '/about', element: <AboutPage /> },
             { path: '/contact', element: <ContactPage /> },
+            { path: '/server-error', element: <ServerError />},
+            { path: '/not-found', element: <NotFound />},
+            { path: '*', element: <Navigate replace to ='/not-found' />}
         ]
     }
 ], {
