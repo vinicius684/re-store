20f4dca6035f786c5ede82bf7d7e5a8da0073b0e
Vinícius Henriques <viniciuspinholi6@gmail.com>
2025-05-26 15:55:10 -0300

    Endpoint to get the filters from the API

--- src/ReStore.API/Controllers/ProductsController.cs
+++ src/ReStore.API/Controllers/ProductsController.cs
@@ -10,15 +10,15 @@ namespace ReStore.API.Controllers
     public class ProductsController(StoreContext context) : BaseApiController //use of primary constructor
     {
         [HttpGet]
-        public async Task<ActionResult<List<Product>>> GetProducts([FromQuery]ProductParams productParams)
+        public async Task<ActionResult<List<Product>>> GetProducts([FromQuery] ProductParams productParams)
         {
             var query = context.Products
                 .Sort(productParams.OrderBy)
                 .Search(productParams.SearchTerm)
                 .Filter(productParams.Brands, productParams.Types)
                 .AsQueryable();
-            
-            var products = await PagedList<Product>.ToPagedList(query, 
+
+            var products = await PagedList<Product>.ToPagedList(query,
                 productParams.PageNumber, productParams.PageSize);
 
             Response.AddPaginationHeader(products.Metadata);
@@ -34,6 +34,15 @@ namespace ReStore.API.Controllers
             if (product == null) return NotFound();
 
             return product;
+        }
+
+        [HttpGet("filters")]
+        public async Task<IActionResult> GetFilters()
+        {
+            var brands = await context.Products.Select(x => x.Brand).Distinct().ToListAsync();
+            var types = await context.Products.Select(x => x.Type).Distinct().ToListAsync();
+
+            return Ok(new { brands, types });
         }
     }
 }
